a970da983860d57611c083b6df2731e05ac2a803
Ivan Leninger <ileninger@live.com>
2026-05-19 21:56:09 +0200

    Day2

--- Learn/Hello/Hello/main.cpp
+++ Learn/Hello/Hello/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-	std::cout << "Hello World" << std::endl;
+int main() {
+
+	const string MY_Name = "Ivan";
+
+	MY_Name="JOZO"
+	cout << MY_Name << endl;
+	
 	return 0;
 }
